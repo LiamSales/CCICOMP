c7a0b4a21e113732113abf05aac0c0fbd1c40805
LiamSales <liam_miguel_sales@dlsu.edu.ph>
2025-09-15 21:46:45 +0800

    chatgpt'd the code refactored almost everything for bit manipulation, will study it

--- icomp.kt
+++ icomp.kt
@@ -1,135 +1,83 @@
-fun toBinary(decimal: Int): String{
-    
-    val binary = StringBuilder()
-    var answer = decimal
-
-    while (answer > 0){
-        binary.insert(0,answer%2)
-        answer/=2
+fun toBinary(decimal: Int): String {
+    if (decimal == 0) return "0"
+    val sb = StringBuilder()
+    var n = decimal
+    while (n > 0) {
+        sb.insert(0, (n and 1).toString()) // take last bit
+        n = n shr 1                        // shift right
     }
-
-    return binary.toString()
-
+    return sb.toString()
 }
 
-fun toOctal(decimal: Int): String{
-    
-    var binary = toBinary(decimal)
-
-    val octal = StringBuilder()
-    
-    if      (binary.length%3 == 1) binary = "00" + binary
-    else if (binary.length%3 == 2) binary = "0" + binary
-  
-    var i = 3;
-    var j = 0;
-
-    while (j<binary.length){
-  
-        val octate = binary.substring(binary.length - i, binary.length - j).toInt()
-
-        val digits = Triple(octate/100, octate%100/10, octate%10)
-
-        octal.insert(0, digits.first * 4 + digits.second * 2 + digits.third)
-
-        i+=3
-        j+=3
-
-     }
-
-     return octal.toString()
-
+fun toOctal(decimal: Int): String {
+    if (decimal == 0) return "0"
+    val sb = StringBuilder()
+    var n = decimal
+    while (n > 0) {
+        sb.insert(0, (n and 7).toString()) // last 3 bits → 0–7
+        n = n shr 3
+    }
+    return sb.toString()
 }
 
-fun toHexa(decimal: Int): String{
-    
-    var binary = toBinary(decimal)
+fun toHexa(decimal: Int): String {
+    if (decimal == 0) return "0"
+    val hexChars = "0123456789ABCDEF"
+    val sb = StringBuilder()
+    var n = decimal
+    while (n > 0) {
+        val digit = n and 15               // last 4 bits (0–15)
+        sb.insert(0, hexChars[digit])
+        n = n shr 4
+    }
+    return sb.toString()
+}
 
-    val hexa = StringBuilder()
-    
-    if      (binary.length%3 == 1) binary = "000" + binary
-    else if (binary.length%3 == 2) binary = "00" + binary
-    else if (binary.length%3 == 3) binary = "0" + binary
-  
-    var i = 4;
-    var j = 0;
+fun onesComp(binary: String): String {
+    val sb = StringBuilder()
+    for (c in binary) {
+        sb.append(if (c == '0') '1' else '0')
+    }
+    return sb.toString()
+}
 
-    while (j<binary.length){
-
-            // so 1000 is 8, 1001 is 9, 1010 is A
-
-        var hexate = binary.substring(binary.length - i, binary.length - j).toInt()
-
-            if (hexate/1000 == 1 && hexate%100 < 0){
-                hexate%=1000
-                var letter = 'A'
-                letter += hexate //??
-                hexa.insert(0,letter)
-                }
-            else {
-            val digits = List(hexate/1000,  , hexate%100/10, hexate%10)
-
-            octal.insert(0, *8 + *4 + *2 +)
+fun twosComp(binary: String): String {
+    val sb = StringBuilder(binary)
+    var carry = true
+    for (i in sb.length - 1 downTo 0) {
+        if (carry) {
+            if (sb[i] == '0') {
+                sb.setCharAt(i, '1')
+                carry = false
+            } else {
+                sb.setCharAt(i, '0')
             }
         }
-        i+=4
-        j+=4
-     }
-     return hexa.toString()
-}
-
-fun onesComp(binary: String): String{
-
-    val onescomp = StringBuilder()
-
-    var i=0
-
-    while(i<binary.length){
-
-//invert
-
-        i++
     }
-
-    return onescomp.toString()
+    return sb.toString()
 }
 
-fun twosComp(binary: String): String{
-
-    val onescomp = StringBuilder()
-
-    var i=0
-
-    while(i<binary.length){
-
-     //wait for 1 then invert   
-
-        i++
-    }
-
-    return onescomp.toString()
-}
-
-fun main(){
-
-    while(true){
-
+fun main() {
+    while (true) {
         print("Please enter the number in base 10: ")
-
         val decimal = readlnOrNull()?.toIntOrNull() ?: 0
 
-        print("\nEnter the base you wish to convert $decimal to: ")
-
+        print("\nEnter the base you wish to convert $decimal to (2, 8, 16): ")
         val toBase = readlnOrNull()?.toIntOrNull() ?: 2
 
         println()
-        
-        when(toBase){
-            2 -> println(toBinary(decimal))
-            8 -> println(toOctal(decimal))
-            // 16 -> println(toHexa(decimal)) 
+        when (toBase) {
+            2 -> println("Binary: " + toBinary(decimal))
+            8 -> println("Octal: " + toOctal(decimal))
+            16 -> println("Hex: " + toHexa(decimal))
+            else -> println("Unsupported base")
         }
 
+        // show complements in binary
+        val bin = toBinary(decimal)
+        println("1's complement: ${onesComp(bin)}")
+        println("2's complement: ${twosComp(bin)}")
+        println()
     }
-
 }
+
