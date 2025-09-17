2f08f477981af728631fe6d197f21210387bbb8b
Liam Miguel V. Sales <liam_miguel_sales@dlsu.edu.ph>
2025-09-17 21:05:53 +0800

    added notes on lessons learned

--- icomp.kt
+++ icomp.kt
@@ -5,9 +5,10 @@ fun toBinary(decimal: Int): String {
     while (n > 0) {
         sb.insert(0, (n and 1).toString()) // take last bit of the full decimal number n and 1 gets the last bit of N, because we do by the number of digits, completely reliant on n 
         n = n shr 1                        // shifts all bits to the right by one place, so  ex: 1101 becomes 0110, this is actually binary dividing by 2 (floor division)
-    }
+    }// notw about shifting, logical shift just rawdogs everything, no relationship since signbit gets messed up. arithmetic shift saves the sign then pads with said sign
     return sb.toString() // already in binary, prev code had the operations done manually, but bitwise operators automate the process directly
 }
+// shitfing can be used in conditionals to get the ith bit (ex: i want the third bit, so i=3 then shift right 3, use and to get, or/not to set
 
 fun toOctal(decimal: Int): String {
     if (decimal == 0) return "0" 
@@ -41,21 +42,38 @@ fun onesComp(binary: String): String {
     return sb.toString()
 }
 
-fun twosComp(binary: String): String { //ones comp +1
-    val sb = StringBuilder(binary) //take note the sb has an argument now, it contains the binary
-    var carry = true // do i still need to add 1 to the next higher bit?
-    for (i in sb.length - 1 downTo 0) { // will continue study tomorrow but this is the last one
+// twos comp is basically the negative version, the MSB is the sign, 0 pos, 1 neg: then what number shall be added to the rest to get 2^x,
+fun twosComp(binary: String): String {
+    // STEP 1: Flip all the bits (one's complement)
+    val flipped = StringBuilder()
+    for (c in binary) {
+        flipped.append(if (c == '0') '1' else '0')
+    }
+
+    //ones comp +1 because that makes it 0 with padded 1s to the right. then +1 is 2s comp. Take note this is dependent on the number of bits, so 8 bit looks different from 16 bit since we count from the left. sign is padded.
+// we already flipped so this is addition
+    // STEP 2: Add 1 
+    val sb = StringBuilder(flipped)
+    var carry = true  // we want to add +1, basically our operand
+
+    for (i in sb.length - 1 downTo 0) { // start from rightmost bit
         if (carry) {
             if (sb[i] == '0') {
                 sb.setCharAt(i, '1')
-                carry = false
-            } else {
-                sb.setCharAt(i, '0')
+                carry = false   // no more carry, stop adding
+            } else { 
+                sb.setCharAt(i, '0') // 1 + 1 = 0 (with carry)
+                carry = true         // keep carry = 1, ripple to next bit
             }
         }
     }
+
+    //  If carry is still true after the loop, overflow is ignored in fixed-width arithmetic
+     if (carry) sb.insert(0, '1')
+
     return sb.toString()
 }
+
 
 fun main() {
     while (true) {
